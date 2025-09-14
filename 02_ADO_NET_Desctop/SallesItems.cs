06aeb52abeda2cec6c5a95f97b3414c205031d77
Johnny <esp25goodlife@gmail.com>
2025-09-14 12:31:20 +0200

    50% is done

---
+++ 02_ADO_NET_Desctop/SallesItems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ADO_NET_Desctop
+{
+    internal class SallesItems
+    {
+        public int Id { get; set; }
+        public int ProductID { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int EmployeeID { get; set; }
+        public int ClientID { get; set; }
+        public DateTime Date { get; set; }
+        override public string ToString()
+        {
+            return $"Id: {Id,5}, ProductID: {ProductID,5}, Price: {Price,20}, Quantity: {Quantity,5}, EmployeeID: {EmployeeID,5}, ClientID: {ClientID,5}, Date: {Date,15}";
+        }
+    }
+}
